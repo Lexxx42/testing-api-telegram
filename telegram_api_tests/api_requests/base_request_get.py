bcaf0969e730c23ef05dc16cb50a9403939cf6fe
Alexander Konukhov <jesusmisha@gmail.com>
2023-03-19 09:24:46 +0300

    separated base class for get methods

--- telegram_api_tests/api_requests/base_request_get.py
+++ telegram_api_tests/api_requests/base_request_get.py
@@ -5,20 +5,9 @@ from .expected_results import ExpectedBaseRequest as ebr
 
 
 class BaseRequestGet(BaseRequest):
-
     def send(self):
         self.response = requests.get(self.url + self.token + '/' + self.method_api)
         return self.response
-
-    def should_be_status_ok(self):
-        assert self.response.json()['ok'] == ebr.STATUS_OK, \
-            f'Response status ok with method {self.method_api} shold be {ebr.STATUS_OK},' \
-            f' but got {self.response.json()["ok"]}'
-
-    def should_be_status_code_200(self):
-        assert self.response.status_code == ebr.STATUS_CODE, \
-            f'Response status code ok with method {self.method_api} shold be {ebr.STATUS_CODE},' \
-            f' but got {self.response.status_code}'
 
     def should_be_correct_id_of_bot(self):
         assert self.response.json()['result']['id'] == ebr.BOT_ID, \
