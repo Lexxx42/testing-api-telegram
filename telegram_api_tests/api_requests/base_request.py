78883d84e03ea961ea15c7e20a8909f9e7b156c2
Alexander Konukhov <jesusmisha@gmail.com>
2023-03-19 09:24:29 +0300

    added some method to base class

--- telegram_api_tests/api_requests/base_request.py
+++ telegram_api_tests/api_requests/base_request.py
@@ -1,5 +1,6 @@
 import requests
 from .loader import BOT_TOKEN
+from .expected_results import ExpectedBaseRequest as ebr
 
 
 class BaseRequest():
@@ -19,3 +20,13 @@ class BaseRequest():
             self.response = requests.post(self.url + self.token + '/' + self.method_api,
                                           data=self.params)
             return self.response
+
+    def should_be_status_ok(self):
+        assert self.response.json()['ok'] == ebr.STATUS_OK, \
+            f'Response status ok with method {self.method_api} shold be {ebr.STATUS_OK},' \
+            f' but got {self.response.json()["ok"]}'
+
+    def should_be_status_code_200(self):
+        assert self.response.status_code == ebr.STATUS_CODE, \
+            f'Response status code ok with method {self.method_api} shold be {ebr.STATUS_CODE},' \
+            f' but got {self.response.status_code}'
