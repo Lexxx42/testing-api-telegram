5b0ec6818a1de10999ba19239cae1fb6fa467d35
Alexander Konukhov <jesusmisha@gmail.com>
2023-03-19 08:02:09 +0300

    expected results separated from tests

---
+++ telegram_api_tests/api_requests/expected_results.py
@@ -0,0 +1,14 @@
+class ExpectedBaseRequest():
+    STATUS_OK = True
+    STATUS_CODE = 200
+
+
+class ExpectedGetMe():
+    BOT_ID = 5623238635
+    IS_BOT = True
+    RESPONSE_FROM_BOT = True
+    BOT_FIRST_NAME = 'testing_monkey'
+    BOT_USERNAME = 'testing_monke_bot'
+    CAN_JOIN_GROUPS = True
+    CAN_READ_ALL_MESSAGES = False
+    IS_SUPPORT_INLINE_QUERIES = False
